6c4bcc7638f762c0cad140853ce45ea3af9bf6bc
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-05-21 00:38:29 -0400

    Add test for bytes_split

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -1,6 +1,7 @@
 import pytest
 
 from multiaddr.codec import size_for_addr
+from multiaddr.codec import bytes_split
 from multiaddr.codec import string_to_bytes
 from multiaddr.codec import bytes_to_string
 
@@ -14,6 +15,14 @@ from multiaddr.protocols import _names_to_protocols
         ])
 def test_size_for_addr(proto, buf, expected):
     assert size_for_addr(proto, buf) == expected
+
+
+@pytest.mark.parametrize("buf, expected", [
+    (b'047f0000011104d2047f0000010610e1', ['\x04\x7f\x00\x00\x01', '\x11\x04\xd2', '\x04\x7f\x00\x00\x01', '\x06\x10\xe1']),  # "/ip4/127.0.0.1/udp/1234/ip4/127.0.0.1/tcp/4321"
+        ])
+def test_bytes_split(buf, expected):
+    print bytes_split(buf)
+    assert (bytes_split(buf) == expected)
 
 
 def test_string_to_bytes():
