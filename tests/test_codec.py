886fb1b12b44c2ea3c9e776799752b28a5d0be3d
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-06-05 18:21:32 -0400

    Fix almost all lines exceeding 80 chars

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -1,11 +1,11 @@
 import pytest
 
-from multiaddr.codec import size_for_addr
-from multiaddr.codec import bytes_split
-from multiaddr.codec import string_to_bytes
-from multiaddr.codec import bytes_to_string
 from multiaddr.codec import address_bytes_to_string
 from multiaddr.codec import address_string_to_bytes
+from multiaddr.codec import bytes_split
+from multiaddr.codec import bytes_to_string
+from multiaddr.codec import size_for_addr
+from multiaddr.codec import string_to_bytes
 
 from multiaddr.protocols import _names_to_protocols
 from multiaddr.protocols import Protocol
@@ -21,7 +21,12 @@ def test_size_for_addr(proto, buf, expected):
 
 
 @pytest.mark.parametrize("buf, expected", [
-    (b'047f0000011104d2047f0000010610e1', [b'\x04\x7f\x00\x00\x01', b'\x11\x04\xd2', b'\x04\x7f\x00\x00\x01', b'\x06\x10\xe1']),  # "/ip4/127.0.0.1/udp/1234/ip4/127.0.0.1/tcp/4321"
+    # "/ip4/127.0.0.1/udp/1234/ip4/127.0.0.1/tcp/4321"
+    (b'047f0000011104d2047f0000010610e1',
+        [b'\x04\x7f\x00\x00\x01',
+         b'\x11\x04\xd2',
+         b'\x04\x7f\x00\x00\x01',
+         b'\x06\x10\xe1']),
         ])
 def test_bytes_split(buf, expected):
     assert (bytes_split(buf) == expected)
@@ -29,10 +34,14 @@ def test_bytes_split(buf, expected):
 
 @pytest.mark.parametrize("proto, buf, expected", [
         (_names_to_protocols['ip4'], b'0a0b0c0d', '10.11.12.13'),
-        (_names_to_protocols['ip6'], b'1aa12bb23cc34dd45ee56ff67ab78ac8', '1aa1:2bb2:3cc3:4dd4:5ee5:6ff6:7ab7:8ac8'),
+        (_names_to_protocols['ip6'], b'1aa12bb23cc34dd45ee56ff67ab78ac8',
+            '1aa1:2bb2:3cc3:4dd4:5ee5:6ff6:7ab7:8ac8'),
         (_names_to_protocols['tcp'], b'abcd', '43981'),
-        (_names_to_protocols['onion'], b'9a18087306369043091f04d2', 'timaq4ygg2iegci7:1234'),
-        (_names_to_protocols['ipfs'], b'221220d52ebb89d85b02a284948203a62ff28389c57c9f42beec4ec20db76a68911c0b', 'QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC'),
+        (_names_to_protocols['onion'], b'9a18087306369043091f04d2',
+            'timaq4ygg2iegci7:1234'),
+        (_names_to_protocols['ipfs'],
+            b'221220d52ebb89d85b02a284948203a62ff28389c57c9f42beec4ec20db76a68911c0b',
+            'QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC'),
         ])
 def test_address_bytes_to_string(proto, buf, expected):
     assert (address_bytes_to_string(proto, buf) == expected)
@@ -40,10 +49,14 @@ def test_address_bytes_to_string(proto, buf, expected):
 
 @pytest.mark.parametrize("proto, expected, string", [
         (_names_to_protocols['ip4'], '0a0b0c0d', '10.11.12.13'),
-        (_names_to_protocols['ip6'], '1aa12bb23cc34dd45ee56ff67ab78ac8', '1aa1:2bb2:3cc3:4dd4:5ee5:6ff6:7ab7:8ac8'),
+        (_names_to_protocols['ip6'], '1aa12bb23cc34dd45ee56ff67ab78ac8',
+            '1aa1:2bb2:3cc3:4dd4:5ee5:6ff6:7ab7:8ac8'),
         (_names_to_protocols['tcp'], b'abcd', '43981'),
-        (_names_to_protocols['onion'], b'9a18087306369043091f04d2', 'timaq4ygg2iegci7:1234'),
-        (_names_to_protocols['ipfs'], b'221220d52ebb89d85b02a284948203a62ff28389c57c9f42beec4ec20db76a68911c0b', 'QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC'),
+        (_names_to_protocols['onion'], b'9a18087306369043091f04d2',
+            'timaq4ygg2iegci7:1234'),
+        (_names_to_protocols['ipfs'],
+            b'221220d52ebb89d85b02a284948203a62ff28389c57c9f42beec4ec20db76a68911c0b',
+            'QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC'),
         ])
 def test_address_string_to_bytes(proto, string, expected):
     assert (address_string_to_bytes(proto, string) == expected)
