8e9836947d65df331aa38beaaaab3caac59f6f78
Fred Thomsen <fredthomsen@fredthomsen.net>
2016-06-21 19:56:50 -0400

    Place duplicated unit test parameters into constant and some lint updates

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -15,7 +15,7 @@ from multiaddr.protocols import Protocol
     (_names_to_protocols['https'], b'\x01\x02\x03', 0),
     (_names_to_protocols['ip4'], b'\x01\x02\x03', 4),
     (_names_to_protocols['ipfs'], b'\x40\x50\x60\x51', 65),
-        ])
+])
 def test_size_for_addr(proto, buf, expected):
     assert size_for_addr(proto, buf) == expected
 
@@ -23,43 +23,36 @@ def test_size_for_addr(proto, buf, expected):
 @pytest.mark.parametrize("buf, expected", [
     # "/ip4/127.0.0.1/udp/1234/ip4/127.0.0.1/tcp/4321"
     (b'047f0000011104d2047f0000010610e1',
-        [b'\x04\x7f\x00\x00\x01',
-         b'\x11\x04\xd2',
-         b'\x04\x7f\x00\x00\x01',
-         b'\x06\x10\xe1']),
-        ])
+     [b'\x04\x7f\x00\x00\x01',
+      b'\x11\x04\xd2',
+      b'\x04\x7f\x00\x00\x01',
+      b'\x06\x10\xe1']),
+])
 def test_bytes_split(buf, expected):
-    assert (bytes_split(buf) == expected)
+    assert bytes_split(buf) == expected
+
+ADDR_BYTES_MAP_STR_TEST_DATA = [
+    (_names_to_protocols['ip4'], b'0a0b0c0d', '10.11.12.13'),
+    (_names_to_protocols['ip6'], b'1aa12bb23cc34dd45ee56ff67ab78ac8',
+     '1aa1:2bb2:3cc3:4dd4:5ee5:6ff6:7ab7:8ac8'),
+    (_names_to_protocols['tcp'], b'abcd', '43981'),
+    (_names_to_protocols['onion'], b'9a18087306369043091f04d2',
+     'timaq4ygg2iegci7:1234'),
+    (_names_to_protocols['ipfs'],
+     b'221220d52ebb89d85b02a284948203a62ff28389c57c9f42beec4ec20db76a68911c0b',
+     'QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC'),
+]
 
 
-@pytest.mark.parametrize("proto, buf, expected", [
-        (_names_to_protocols['ip4'], b'0a0b0c0d', '10.11.12.13'),
-        (_names_to_protocols['ip6'], b'1aa12bb23cc34dd45ee56ff67ab78ac8',
-            '1aa1:2bb2:3cc3:4dd4:5ee5:6ff6:7ab7:8ac8'),
-        (_names_to_protocols['tcp'], b'abcd', '43981'),
-        (_names_to_protocols['onion'], b'9a18087306369043091f04d2',
-            'timaq4ygg2iegci7:1234'),
-        (_names_to_protocols['ipfs'],
-            b'221220d52ebb89d85b02a284948203a62ff28389c57c9f42beec4ec20db76a68911c0b',
-            'QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC'),
-        ])
+@pytest.mark.parametrize("proto, buf, expected", ADDR_BYTES_MAP_STR_TEST_DATA)
 def test_address_bytes_to_string(proto, buf, expected):
-    assert (address_bytes_to_string(proto, buf) == expected)
+    assert address_bytes_to_string(proto, buf) == expected
 
 
-@pytest.mark.parametrize("proto, expected, string", [
-        (_names_to_protocols['ip4'], '0a0b0c0d', '10.11.12.13'),
-        (_names_to_protocols['ip6'], '1aa12bb23cc34dd45ee56ff67ab78ac8',
-            '1aa1:2bb2:3cc3:4dd4:5ee5:6ff6:7ab7:8ac8'),
-        (_names_to_protocols['tcp'], b'abcd', '43981'),
-        (_names_to_protocols['onion'], b'9a18087306369043091f04d2',
-            'timaq4ygg2iegci7:1234'),
-        (_names_to_protocols['ipfs'],
-            b'221220d52ebb89d85b02a284948203a62ff28389c57c9f42beec4ec20db76a68911c0b',
-            'QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC'),
-        ])
+@pytest.mark.parametrize("proto, expected, string",
+                         ADDR_BYTES_MAP_STR_TEST_DATA)
 def test_address_string_to_bytes(proto, string, expected):
-    assert (address_string_to_bytes(proto, string) == expected)
+    assert address_string_to_bytes(proto, string) == expected
 
 
 def test_string_to_bytes():
@@ -78,9 +71,9 @@ def test_bytes_to_string():
 
 
 @pytest.mark.parametrize("string", [
-        'test',
-        '/ip4/'
-    ])
+    'test',
+    '/ip4/'
+])
 def test_string_to_bytes_value_error(string):
     with pytest.raises(ValueError):
         string_to_bytes(string)
@@ -95,27 +88,27 @@ class DummyProtocol(Protocol):
 
 
 @pytest.mark.parametrize("proto, address", [
-        (DummyProtocol(234, 32, 'test', b'123'), '1.2.3.4'),
-        (_names_to_protocols['ip4'], '1124.2.3'),
-        (_names_to_protocols['ip6'], '123:31224444'),
-        (_names_to_protocols['tcp'], 'a'),
-        (_names_to_protocols['tcp'], '100000'),
-        (_names_to_protocols['onion'], '100000'),
-        (_names_to_protocols['onion'], '1234567890123456:0'),
-        (_names_to_protocols['onion'], 'timaq4ygg2iegci7:a'),
-        (_names_to_protocols['onion'], 'timaq4ygg2iegci7:0'),
-        (_names_to_protocols['onion'], 'timaq4ygg2iegci7:71234'),
-        (_names_to_protocols['ipfs'], '15230d52ebb89d85b02a284948203a'),
-        ])
+    (DummyProtocol(234, 32, 'test', b'123'), '1.2.3.4'),
+    (_names_to_protocols['ip4'], '1124.2.3'),
+    (_names_to_protocols['ip6'], '123:31224444'),
+    (_names_to_protocols['tcp'], 'a'),
+    (_names_to_protocols['tcp'], '100000'),
+    (_names_to_protocols['onion'], '100000'),
+    (_names_to_protocols['onion'], '1234567890123456:0'),
+    (_names_to_protocols['onion'], 'timaq4ygg2iegci7:a'),
+    (_names_to_protocols['onion'], 'timaq4ygg2iegci7:0'),
+    (_names_to_protocols['onion'], 'timaq4ygg2iegci7:71234'),
+    (_names_to_protocols['ipfs'], '15230d52ebb89d85b02a284948203a'),
+])
 def test_address_string_to_bytes_value_error(proto, address):
     with pytest.raises(ValueError):
         address_string_to_bytes(proto, address)
 
 
 @pytest.mark.parametrize("proto, buf", [
-        (DummyProtocol(234, 32, 'test', b'123'), b'0a0b0c0d'),
-        (_names_to_protocols['ipfs'], b'15230d52ebb89d85b02a284948203a')
-        ])
+    (DummyProtocol(234, 32, 'test', b'123'), b'0a0b0c0d'),
+    (_names_to_protocols['ipfs'], b'15230d52ebb89d85b02a284948203a')
+])
 def test_address_bytes_to_string_value_error(proto, buf):
     with pytest.raises(ValueError):
         address_bytes_to_string(proto, buf)
